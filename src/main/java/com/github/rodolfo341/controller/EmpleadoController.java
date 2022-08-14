4a87288169b9e615712c41d5b3c1aeeb44cb7f7f
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-08-14 15:32:01 -0400

    agregamos enlace con el frontend mediante la anotacion crossorigin en el controlados

--- src/main/java/com/github/rodolfo341/controller/EmpleadoController.java
+++ src/main/java/com/github/rodolfo341/controller/EmpleadoController.java
@@ -3,6 +3,7 @@ package com.github.rodolfo341.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.github.rodolfo341.repository.IEmpleadoRepository;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class EmpleadoController {
 
 	@Autowired
