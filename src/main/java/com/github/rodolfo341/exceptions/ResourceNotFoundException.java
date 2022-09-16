59ec39d036c3eb258c23444fbdbacd550b4f038f
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-09-15 21:41:22 -0300

    corrigiendo

--- src/main/java/com/github/rodolfo341/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/github/rodolfo341/exceptions/ResourceNotFoundException.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value =  HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {	
-		private static final Long serialVersionID = 1L;		
-		public ResourceNotFoundException(String mensaje) {
-			super(mensaje);
-		}
+
+	private static final Long serialVersionID = 1L;		
+	
+	public ResourceNotFoundException(String mensaje) {
+		super(mensaje);
+	}
 }
--- src/main/java/com/github/rodolfo341/repository/IEmpleadoRepository.java
+++ src/main/java/com/github/rodolfo341/repository/IEmpleadoRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.github.rodolfo341.model.Empleado;
 
 @Repository
-public interface IEmpleadoRepository extends JpaRepository<Empleado, Long> { }
+public interface IEmpleadoRepository extends JpaRepository<Empleado, Long> { 
+	
+}
